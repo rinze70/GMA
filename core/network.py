e8b3011cf27de48d1699d29ed05cb86552c0118a
gohiy <57204793+rinze70@users.noreply.github.com>
2022-11-10 18:10:22 +0000

    Fix bug.

--- core/network.py
+++ core/network.py
@@ -111,7 +111,7 @@ class RAFTGMA(nn.Module):
         for itr in range(iters):
             coords1 = coords1.detach()
             corr = corr_fn(coords1)  # index correlation volume
-            if self.args.eca:
+            if self.args.corr_eca:
                 corr = corr + self.eca(corr) * self.eca_gamma # Efficient Channel Attention: https://arxiv.org/abs/1910.03151
 
             flow = coords1 - coords0
