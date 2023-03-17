07b1a3d3ceb88619a67bd52348394b8a211cc311
rinze70 <gohiy@outlook.com>
2023-03-17 19:37:07 +0800

    Add davit channel attention.

---
+++ core/davit.py
@@ -0,0 +1,134 @@
+from typing import Tuple
+
+import torch
+from torch import nn
+
+from timm.models.layers import DropPath, to_2tuple
+
+class ConvPosEnc(nn.Module):
+    """Depth-wise convolution to get the positional information.
+    """
+    def __init__(self, dim, k=3):
+        super(ConvPosEnc, self).__init__()
+        self.proj = nn.Conv2d(dim,
+                              dim,
+                              to_2tuple(k),
+                              to_2tuple(1),
+                              to_2tuple(k // 2),
+                              groups=dim)
+
+    def forward(self, x, size: Tuple[int, int]):
+        B, N, C = x.shape
+        H, W = size
+        assert N == H * W
+
+        feat = x.transpose(1, 2).view(B, C, H, W)
+        feat = self.proj(feat)
+        feat = feat.flatten(2).transpose(1, 2)
+        x = x + feat
+        return x
+
+class Mlp(nn.Module):
+    """ MLP as used in Vision Transformer, MLP-Mixer and related networks
+    """
+    def __init__(
+            self,
+            in_features,
+            hidden_features=None,
+            out_features=None,
+            act_layer=nn.GELU):
+        super().__init__()
+        out_features = out_features or in_features
+        hidden_features = hidden_features or in_features
+        self.fc1 = nn.Linear(in_features, hidden_features)
+        self.act = act_layer()
+        self.fc2 = nn.Linear(hidden_features, out_features)
+
+    def forward(self, x):
+        x = self.fc1(x)
+        x = self.act(x)
+        x = self.fc2(x)
+        return x
+       
+class ChannelAttention(nn.Module):
+    r""" Channel based self attention.
+    Args:
+        dim (int): Number of input channels.
+        num_heads (int): Number of the groups.
+        qkv_bias (bool, optional):  If True, add a learnable bias to query, key, value. Default: True
+    """
+
+    def __init__(self, dim, num_heads=8, qkv_bias=False):
+        super().__init__()
+        self.num_heads = num_heads
+        head_dim = dim // num_heads
+        self.scale = head_dim ** -0.5
+        self.qkv = nn.Linear(dim, dim * 3, bias=qkv_bias)
+        self.proj = nn.Linear(dim, dim)
+
+    def forward(self, x):
+        B, N, C = x.shape
+
+        qkv = self.qkv(x).reshape(B, N, 3, self.num_heads, C // self.num_heads).permute(2, 0, 3, 1, 4) # 3, B, h, N, C 
+        q, k, v = qkv[0], qkv[1], qkv[2]
+
+        k = k * self.scale
+        attention = k.transpose(-1, -2) @ v
+        attention = attention.softmax(dim=-1)
+        x = (attention @ q.transpose(-1, -2)).transpose(-1, -2)
+        x = x.transpose(1, 2).reshape(B, N, C)
+        x = self.proj(x)
+        return x
+
+class ChannelBlock(nn.Module):
+    r""" Channel-wise Local Transformer Block.
+    Args:
+        dim (int): Number of input channels.
+        num_heads (int): Number of attention heads.
+        mlp_ratio (float): Ratio of mlp hidden dim to embedding dim.
+        qkv_bias (bool, optional): If True, add a learnable bias to query, key, value. Default: True
+        drop_path (float, optional): Stochastic depth rate. Default: 0.0
+        act_layer (nn.Module, optional): Activation layer. Default: nn.GELU
+        norm_layer (nn.Module, optional): Normalization layer.  Default: nn.LayerNorm
+        ffn (bool): If False, pure attention network without FFNs
+    """
+    def __init__(self, dim, num_heads, mlp_ratio=4., qkv_bias=False,
+                 drop_path=0., act_layer=nn.GELU, norm_layer=nn.LayerNorm,
+                 ffn=True):
+        super().__init__()
+
+        self.cpe = nn.ModuleList([ConvPosEnc(dim=dim, k=3),
+                                  ConvPosEnc(dim=dim, k=3)])
+        self.ffn = ffn
+        self.norm1 = norm_layer(dim)
+        self.attn = ChannelAttention(dim, num_heads=num_heads, qkv_bias=qkv_bias)
+        self.drop_path = DropPath(drop_path) if drop_path > 0. else nn.Identity()
+
+        if self.ffn:
+            self.norm2 = norm_layer(dim)
+            mlp_hidden_dim = int(dim * mlp_ratio)
+            self.mlp = Mlp(
+                in_features=dim,
+                hidden_features=mlp_hidden_dim,
+                act_layer=act_layer)
+
+    def forward(self, x, size):
+        x = self.cpe[0](x, size)
+        cur = self.norm1(x)
+        cur = self.attn(cur)
+        x = x + self.drop_path(cur)
+
+        x = self.cpe[1](x, size)
+        if self.ffn:
+            x = x + self.drop_path(self.mlp(self.norm2(x)))
+        return x, size
+
+if __name__ == '__main__':
+    from einops import rearrange
+    h, w = 368//8, 496//8
+    x = torch.randn(2, 256, h, w)
+    x = rearrange(x, 'n c h w -> n (h w) c')
+    cb = ChannelBlock(dim=256, num_heads=8)
+    x, size = cb(x, (h, w))
+    x = rearrange(x, 'n (h w) c -> n c h w', h=h, w=w)
+    print(x.shape)
