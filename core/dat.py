3354565c9b1adf894d41b76ae6782b06c022e7d4
rinze70 <gohiy@outlook.com>
2023-03-23 17:46:58 +0800

    Add dat corr.

--- core/dat.py
+++ core/dat.py
@@ -185,7 +185,109 @@ class LayerNormProxy(nn.Module):
         x = einops.rearrange(x, 'b c h w -> b h w c')
         x = self.norm(x)
         return einops.rearrange(x, 'b h w c -> b c h w')
-    
+
+class DACorr(DAttentionBaseline):
+        
+        def __init__(self, q_size, 
+                    kv_size, 
+                    n_heads, 
+                    n_head_channels, 
+                    n_groups=1,
+                    attn_drop=0.0, 
+                    proj_drop=0.0, 
+                    stride=1, 
+                    offset_range_factor=2, 
+                    use_pe=True, 
+                    dwc_pe=False,
+                    no_off=False, 
+                    fixed_pe=False, 
+                    stage_idx=3):
+            
+            super().__init__(q_size, kv_size, n_heads, n_head_channels, n_groups,
+                    attn_drop, proj_drop, stride, 
+                    offset_range_factor, use_pe, dwc_pe,
+                    no_off, fixed_pe, stage_idx)
+            
+        def forward(self, x, y):
+            
+            B, C, H, W = x.size()
+            dtype, device = x.dtype, x.device
+            
+            q = self.proj_q(x)
+            q_off = einops.rearrange(q, 'b (g c) h w -> (b g) c h w', g=self.n_groups, c=self.n_group_channels)
+            offset = self.conv_offset(q_off) # B * g 2 Hg Wg
+            Hk, Wk = offset.size(2), offset.size(3)
+            n_sample = Hk * Wk
+            
+            if self.offset_range_factor > 0:
+                offset_range = torch.tensor([1.0 / Hk, 1.0 / Wk], device=device).reshape(1, 2, 1, 1)
+                offset = offset.tanh().mul(offset_range).mul(self.offset_range_factor)
+                
+            offset = einops.rearrange(offset, 'b p h w -> b h w p')
+            reference = self._get_ref_points(Hk, Wk, B, dtype, device)
+                
+            if self.no_off:
+                offset = offset.fill(0.0)
+                
+            if self.offset_range_factor >= 0:
+                pos = offset + reference
+            else:
+                pos = (offset + reference).tanh()
+            
+            x_sampled = F.grid_sample(
+                input=y.reshape(B * self.n_groups, self.n_group_channels, H, W), 
+                grid=pos[..., (1, 0)], # y, x -> x, y
+                mode='bilinear', align_corners=True) # B * g, Cg, Hg, Wg
+                
+            x_sampled = x_sampled.reshape(B, C, 1, n_sample)
+
+            q = q.reshape(B * self.n_heads, self.n_head_channels, H * W)
+            k = self.proj_k(x_sampled).reshape(B * self.n_heads, self.n_head_channels, n_sample)
+            # v = self.proj_v(x_sampled).reshape(B * self.n_heads, self.n_head_channels, n_sample)
+            
+            attn = torch.einsum('b c m, b c n -> b m n', q, k) # B * h, HW, Ns
+            attn = attn.mul(self.scale)
+            
+            if self.use_pe:
+                
+                if self.dwc_pe:
+                    residual_lepe = self.rpe_table(q.reshape(B, C, H, W)).reshape(B * self.n_heads, self.n_head_channels, H * W)
+                elif self.fixed_pe:
+                    rpe_table = self.rpe_table
+                    attn_bias = rpe_table[None, ...].expand(B, -1, -1, -1)
+                    attn = attn + attn_bias.reshape(B * self.n_heads, H * W, self.n_sample)
+                else:
+                    rpe_table = self.rpe_table
+                    rpe_bias = rpe_table[None, ...].expand(B, -1, -1, -1)
+                    
+                    q_grid = self._get_ref_points(H, W, B, dtype, device)
+                    
+                    displacement = (q_grid.reshape(B * self.n_groups, H * W, 2).unsqueeze(2) - pos.reshape(B * self.n_groups, n_sample, 2).unsqueeze(1)).mul(0.5)
+                    
+                    attn_bias = F.grid_sample(
+                        input=rpe_bias.reshape(B * self.n_groups, self.n_group_heads, 2 * H - 1, 2 * W - 1),
+                        grid=displacement[..., (1, 0)],
+                        mode='bilinear', align_corners=True
+                    ) # B * g, h_g, HW, Ns
+                    
+                    attn_bias = attn_bias.reshape(B * self.n_heads, H * W, n_sample)
+                    
+                    attn = attn + attn_bias
+
+            # attn = F.softmax(attn, dim=2)
+            # attn = self.attn_drop(attn)
+            
+            # out = torch.einsum('b m n, b c n -> b c m', attn, v)
+            
+            # if self.use_pe and self.dwc_pe:
+            #     out = out + residual_lepe
+            # out = out.reshape(B, C, H, W)
+            
+            # y = self.proj_drop(self.proj_out(out))
+            
+            # return y, pos.reshape(B, self.n_groups, Hk, Wk, 2), reference.reshape(B, self.n_groups, Hk, Wk, 2)
+            return attn.view(B, H, W, 1, H, W)
+
 if __name__ == "__main__":
     fmap1 = torch.randn(1, 3, 26, 42)
     fmap2 = torch.randn(1, 3, 26, 42)
@@ -193,20 +295,7 @@ if __name__ == "__main__":
     dim = fmap1.shape[1]
     heads = 1
     hc = dim // heads
-    n_groups = 1
-    attn_drop = 0.0
-    proj_drop = 0.0
-    stride = 1
-    offset_range_factor = 2
-    use_pe = True
-    dwc_pe = False
-    no_off = False
-    fixed_pe = False
-    stage_idx = 2
-    DAB = DAttentionBaseline(fmap_size, fmap_size, heads, 
-                    hc, n_groups, attn_drop, proj_drop, 
-                    stride, offset_range_factor, use_pe, dwc_pe, 
-                    no_off, fixed_pe, stage_idx)
+    DAC = DACorr(fmap_size, fmap_size, heads, hc)
     
-    out, _, _ = DAB(fmap1)
+    out = DAC(fmap1, fmap2)
     print(out.shape)
