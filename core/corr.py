ac9f55c62d4a00dff80099e216e002adc637c2d0
rinze70 <gohiy@outlook.com>
2023-04-06 22:48:32 +0800

    Add MutiHeadCorrBlock.

--- core/corr.py
+++ core/corr.py
@@ -101,3 +101,61 @@ class CorrBlockSingleScale(nn.Module):
         corr = torch.matmul(fmap1.transpose(1, 2), fmap2)
         corr = corr.view(batch, ht, wd, 1, ht, wd)
         return corr / torch.sqrt(torch.tensor(dim).float())
+
+class MutiHeadCorrBlock:
+    def __init__(self, fmap1, fmap2, num_levels=4, radius=4):
+        self.num_levels = num_levels
+        self.radius = radius
+        self.corr_pyramid = []
+
+        # all pairs correlation
+        corr = MutiHeadCorrBlock.corr(fmap1, fmap2, num_levels)
+
+        batch, h1, w1, dim, h2, w2 = corr.shape
+        corr = corr.reshape(batch * h1 * w1, dim, h2, w2)
+
+        self.corr_pyramid.append(corr[:,0].view(batch * h1 * w1, 1, h2, w2))
+        for i in range(1, self.num_levels):
+            cor = corr[:,i].view(batch * h1 * w1, 1, h2, w2)
+            cor = F.avg_pool2d(cor, 2**i, stride=2**i)
+            self.corr_pyramid.append(cor)
+
+    def __call__(self, coords):
+        r = self.radius
+        coords = coords.permute(0, 2, 3, 1)
+        batch, h1, w1, _ = coords.shape
+
+        out_pyramid = []
+        for i in range(self.num_levels):
+            corr = self.corr_pyramid[i]
+            dx = torch.linspace(-r, r, 2 * r + 1)
+            dy = torch.linspace(-r, r, 2 * r + 1)
+            delta = torch.stack(torch.meshgrid(dy, dx), axis=-1).to(coords.device)
+
+            centroid_lvl = coords.reshape(batch * h1 * w1, 1, 1, 2) / 2 ** i
+            delta_lvl = delta.view(1, 2 * r + 1, 2 * r + 1, 2)
+            coords_lvl = centroid_lvl + delta_lvl
+
+            corr = bilinear_sampler(corr, coords_lvl)
+            corr = corr.view(batch, h1, w1, -1)
+            out_pyramid.append(corr)
+
+        out = torch.cat(out_pyramid, dim=-1)
+        return out.permute(0, 3, 1, 2).contiguous().float()
+
+    @staticmethod
+    def corr(fmap1, fmap2, hd=4):
+        batch, dim, ht, wd = fmap1.shape
+        fmap1 = fmap1.view(batch, hd, dim//hd, ht * wd)
+        fmap2 = fmap2.view(batch, hd, dim//hd, ht * wd)
+
+        corr = torch.matmul(fmap1.transpose(2, 3), fmap2).permute(0, 2, 1, 3)
+        corr = corr.view(batch, ht, wd, hd, ht, wd)
+        return corr / torch.sqrt(torch.tensor(dim//hd).float())
+
+if __name__ == "__main__":
+    fmap1 = torch.randn(1, 256, 368//8, 496//8)
+    fmap2 = torch.randn(1, 256, 368//8, 496//8)
+    MHCB = MutiHeadCorrBlock(fmap1, fmap2, num_levels=4, radius=4)
+    # CB = CorrBlock(fmap1, fmap2, num_levels=4, radius=4)
+    # print(corr.shape)
