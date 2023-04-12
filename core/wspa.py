70a7179e1bf878046463dbc37bec8b421c7e7d4d
rinze70 <gohiy@outlook.com>
2023-04-12 15:32:02 +0800

    Add self and cross attention.

---
+++ core/wspa.py
@@ -0,0 +1,448 @@
+from typing import Tuple
+
+import torch
+from torch import nn
+import torch.nn.functional as F
+
+from timm.models.layers import DropPath, to_2tuple
+
+
+class ConvPosEnc(nn.Module):
+    """Depth-wise convolution to get the positional information.
+    """
+    def __init__(self, dim, k=3):
+        super(ConvPosEnc, self).__init__()
+        self.proj = nn.Conv2d(dim,
+                              dim,
+                              to_2tuple(k),
+                              to_2tuple(1),
+                              to_2tuple(k // 2),
+                              groups=dim)
+
+    def forward(self, x, size: Tuple[int, int]):
+        B, N, C = x.shape
+        H, W = size
+        assert N == H * W
+
+        feat = x.transpose(1, 2).view(B, C, H, W)
+        feat = self.proj(feat)
+        feat = feat.flatten(2).transpose(1, 2)
+        x = x + feat
+        return x
+
+class Mlp(nn.Module):
+    """ MLP as used in Vision Transformer, MLP-Mixer and related networks
+    """
+    def __init__(
+            self,
+            in_features,
+            hidden_features=None,
+            out_features=None,
+            act_layer=nn.GELU):
+        super().__init__()
+        out_features = out_features or in_features
+        hidden_features = hidden_features or in_features
+        self.fc1 = nn.Linear(in_features, hidden_features)
+        self.act = act_layer()
+        self.fc2 = nn.Linear(hidden_features, out_features)
+
+    def forward(self, x):
+        x = self.fc1(x)
+        x = self.act(x)
+        x = self.fc2(x)
+        return x
+    
+def window_partition(x, window_size: int):
+    """
+    Args:
+        x: (B, H, W, C)
+        window_size (int): window size
+
+    Returns:
+        windows: (num_windows*B, window_size, window_size, C)
+    """
+    B, H, W, C = x.shape
+    x = x.view(B, H // window_size, window_size, W // window_size, window_size, C)
+    windows = x.permute(0, 1, 3, 2, 4, 5).contiguous().view(-1, window_size, window_size, C)
+    return windows
+
+
+def window_reverse(windows, window_size: int, H: int, W: int):
+    """
+    Args:
+        windows: (num_windows*B, window_size, window_size, C)
+        window_size (int): Window size
+        H (int): Height of image
+        W (int): Width of image
+
+    Returns:
+        x: (B, H, W, C)
+    """
+    B = int(windows.shape[0] / (H * W / window_size / window_size))
+    x = windows.view(B, H // window_size, W // window_size, window_size, window_size, -1)
+    x = x.permute(0, 1, 3, 2, 4, 5).contiguous().view(B, H, W, -1)
+    return x
+
+class WindowAttention(nn.Module):
+    r""" Window based multi-head self attention (W-MSA) module.
+
+    Args:
+        dim (int): Number of input channels.
+        window_size (tuple[int]): The height and width of the window.
+        num_heads (int): Number of attention heads.
+        qkv_bias (bool, optional):  If True, add a learnable bias to query, key, value. Default: True
+    """
+
+    def __init__(self, dim, window_size, num_heads, qkv_bias=True):
+
+        super().__init__()
+        self.dim = dim
+        self.window_size = window_size
+        self.num_heads = num_heads
+        head_dim = dim // num_heads
+        self.scale = head_dim ** -0.5
+
+        self.qkv = nn.Linear(dim, dim * 3, bias=qkv_bias)
+        self.proj = nn.Linear(dim, dim)
+
+        self.softmax = nn.Softmax(dim=-1)
+
+    def forward(self, x):
+        B_, N, C = x.shape
+        qkv = self.qkv(x).reshape(B_, N, 3, self.num_heads, C // self.num_heads).permute(2, 0, 3, 1, 4)
+        q, k, v = qkv[0], qkv[1], qkv[2]
+
+        q = q * self.scale
+        attn = (q @ k.transpose(-2, -1))
+        attn = self.softmax(attn)
+
+        x = (attn @ v).transpose(1, 2).reshape(B_, N, C)
+        x = self.proj(x)
+        return x
+    
+class SpatialBlock(nn.Module):
+    r""" Spatial-wise Local Transformer Block.
+
+    Args:
+        dim (int): Number of input channels.
+        num_heads (int): Number of attention heads.
+        window_size (int): Window size.
+        mlp_ratio (float): Ratio of mlp hidden dim to embedding dim.
+        qkv_bias (bool, optional): If True, add a learnable bias to query, key, value. Default: True
+        drop_path (float, optional): Stochastic depth rate. Default: 0.0
+        act_layer (nn.Module, optional): Activation layer. Default: nn.GELU
+        norm_layer (nn.Module, optional): Normalization layer.  Default: nn.LayerNorm
+        ffn (bool): If False, pure attention network without FFNs
+    """
+
+    def __init__(self, dim, num_heads, window_size=7,
+                 mlp_ratio=4., qkv_bias=True, drop_path=0.,
+                 act_layer=nn.GELU, norm_layer=nn.LayerNorm,
+                 ffn=True):
+        super().__init__()
+        self.dim = dim
+        self.ffn = ffn
+        self.num_heads = num_heads
+        self.window_size = window_size
+        self.mlp_ratio = mlp_ratio
+        self.cpe = nn.ModuleList([ConvPosEnc(dim=dim, k=3),
+                                  ConvPosEnc(dim=dim, k=3)])
+
+        self.norm1 = norm_layer(dim)
+        self.attn = WindowAttention(
+            dim,
+            window_size=to_2tuple(self.window_size),
+            num_heads=num_heads,
+            qkv_bias=qkv_bias)
+
+        self.drop_path = DropPath(drop_path) if drop_path > 0. else nn.Identity()
+
+        if self.ffn:
+            self.norm2 = norm_layer(dim)
+            mlp_hidden_dim = int(dim * mlp_ratio)
+            self.mlp = Mlp(
+                in_features=dim,
+                hidden_features=mlp_hidden_dim,
+                act_layer=act_layer)
+
+    def forward(self, x, size):
+        H, W = size
+        B, L, C = x.shape
+        assert L == H * W, "input feature has wrong size"
+
+        shortcut = self.cpe[0](x, size)
+        x = self.norm1(shortcut)
+        x = x.view(B, H, W, C)
+
+        pad_l = pad_t = 0
+        pad_r = (self.window_size - W % self.window_size) % self.window_size
+        pad_b = (self.window_size - H % self.window_size) % self.window_size
+        x = F.pad(x, (0, 0, pad_l, pad_r, pad_t, pad_b))
+        _, Hp, Wp, _ = x.shape
+
+        x_windows = window_partition(x, self.window_size)
+        x_windows = x_windows.view(-1, self.window_size * self.window_size, C)
+
+        attn_windows = self.attn(x_windows)
+
+        attn_windows = attn_windows.view(-1,
+                                         self.window_size,
+                                         self.window_size,
+                                         C)
+        x = window_reverse(attn_windows, self.window_size, Hp, Wp)
+
+        if pad_r > 0 or pad_b > 0:
+            x = x[:, :H, :W, :].contiguous()
+
+        x = x.view(B, H * W, C)
+        x = shortcut + self.drop_path(x)
+
+        x = self.cpe[1](x, size)
+        if self.ffn:
+            x = x + self.drop_path(self.mlp(self.norm2(x)))
+        return x, size
+
+class ChannelAttention(nn.Module):
+    r""" Channel based self attention.
+
+    Args:
+        dim (int): Number of input channels.
+        num_heads (int): Number of the groups.
+        qkv_bias (bool, optional):  If True, add a learnable bias to query, key, value. Default: True
+    """
+
+    def __init__(self, dim, num_heads=8, qkv_bias=False):
+        super().__init__()
+        self.num_heads = num_heads
+        head_dim = dim // num_heads
+        self.scale = head_dim ** -0.5
+        self.qkv = nn.Linear(dim, dim * 3, bias=qkv_bias)
+        self.proj = nn.Linear(dim, dim)
+
+    def forward(self, x):
+        B, N, C = x.shape
+
+        qkv = self.qkv(x).reshape(B, N, 3, self.num_heads, C // self.num_heads).permute(2, 0, 3, 1, 4)
+        q, k, v = qkv[0], qkv[1], qkv[2]
+
+        k = k * self.scale
+        attention = k.transpose(-1, -2) @ v
+        attention = attention.softmax(dim=-1)
+        x = (attention @ q.transpose(-1, -2)).transpose(-1, -2)
+        x = x.transpose(1, 2).reshape(B, N, C)
+        x = self.proj(x)
+        return x
+
+
+class ChannelBlock(nn.Module):
+    r""" Channel-wise Local Transformer Block.
+
+    Args:
+        dim (int): Number of input channels.
+        num_heads (int): Number of attention heads.
+        mlp_ratio (float): Ratio of mlp hidden dim to embedding dim.
+        qkv_bias (bool, optional): If True, add a learnable bias to query, key, value. Default: True
+        drop_path (float, optional): Stochastic depth rate. Default: 0.0
+        act_layer (nn.Module, optional): Activation layer. Default: nn.GELU
+        norm_layer (nn.Module, optional): Normalization layer.  Default: nn.LayerNorm
+        ffn (bool): If False, pure attention network without FFNs
+    """
+    def __init__(self, dim, num_heads, mlp_ratio=4., qkv_bias=False,
+                 drop_path=0., act_layer=nn.GELU, norm_layer=nn.LayerNorm,
+                 ffn=True):
+        super().__init__()
+
+        self.cpe = nn.ModuleList([ConvPosEnc(dim=dim, k=3),
+                                  ConvPosEnc(dim=dim, k=3)])
+        self.ffn = ffn
+        self.norm1 = norm_layer(dim)
+        self.attn = ChannelAttention(dim, num_heads=num_heads, qkv_bias=qkv_bias)
+        self.drop_path = DropPath(drop_path) if drop_path > 0. else nn.Identity()
+
+        if self.ffn:
+            self.norm2 = norm_layer(dim)
+            mlp_hidden_dim = int(dim * mlp_ratio)
+            self.mlp = Mlp(
+                in_features=dim,
+                hidden_features=mlp_hidden_dim,
+                act_layer=act_layer)
+
+    def forward(self, x, size):
+        x = self.cpe[0](x, size)
+        cur = self.norm1(x)
+        cur = self.attn(cur)
+        x = x + self.drop_path(cur)
+
+        x = self.cpe[1](x, size)
+        if self.ffn:
+            x = x + self.drop_path(self.mlp(self.norm2(x)))
+        return x, size
+
+class CrossChannelAttention(nn.Module):
+    r""" Channel based self attention.
+    Args:
+        dim (int): Number of input channels.
+        num_heads (int): Number of the groups.
+        qkv_bias (bool, optional):  If True, add a learnable bias to query, key, value. Default: True
+    """
+
+    def __init__(self, dim, num_heads=8, qkv_bias=False):
+        super().__init__()
+        self.num_heads = num_heads
+        head_dim = dim // num_heads
+        self.scale = head_dim ** -0.5
+        self.q = nn.Linear(dim, dim, bias=qkv_bias)
+        self.kv = nn.Linear(dim, dim * 2, bias=qkv_bias)
+        self.proj = nn.Linear(dim, dim)
+
+    def forward(self, x, source):
+        B, N, C = x.shape
+
+        q = self.q(x).reshape(B, N, 1, self.num_heads, C // self.num_heads).permute(2, 0, 3, 1, 4) # 1, B, h, N, C 
+        kv = self.kv(source).reshape(B, N, 2, self.num_heads, C // self.num_heads).permute(2, 0, 3, 1, 4) # 2, B, h, N, C 
+        q = q[0]
+        k, v = kv[0], kv[1]
+
+        k = k * self.scale
+        attention = q.transpose(-1, -2) @ k
+        attention = attention.softmax(dim=-1)
+        x = (attention @ v.transpose(-1, -2)).transpose(-1, -2)
+        x = x.transpose(1, 2).reshape(B, N, C)
+        x = self.proj(x)
+        return x
+
+class CrossSpatialAttention(nn.Module):
+    r""" Channel based self attention.
+    Args:
+        dim (int): Number of input channels.
+        num_heads (int): Number of the groups.
+        qkv_bias (bool, optional):  If True, add a learnable bias to query, key, value. Default: True
+    """
+
+    def __init__(self, dim, num_heads=8, qkv_bias=False):
+        super().__init__()
+        self.num_heads = num_heads
+        head_dim = dim // num_heads
+        self.scale = head_dim ** -0.5
+        self.q = nn.Linear(dim, dim, bias=qkv_bias)
+        self.kv = nn.Linear(dim, dim * 2, bias=qkv_bias)
+        self.proj = nn.Linear(dim, dim)
+
+    def forward(self, x, source):
+        B, N, C = x.shape
+
+        q = self.q(x).reshape(B, N, 1, self.num_heads, C // self.num_heads).permute(2, 0, 3, 1, 4) # 1, B, h, N, C 
+        kv = self.kv(source).reshape(B, N, 2, self.num_heads, C // self.num_heads).permute(2, 0, 3, 1, 4) # 2, B, h, N, C 
+        q = q[0]
+        k, v = kv[0], kv[1]
+
+        q = q * self.scale
+        attention = q @ k.transpose(-1, -2)
+        attention = attention.softmax(dim=-1)
+        x = (attention @ v)
+        x = x.transpose(1, 2).reshape(B, N, C)
+        x = self.proj(x)
+        return x
+    
+class CrossAttentionBlock(nn.Module):
+    r""" Channel-wise Local Transformer Block.
+    Args:
+        dim (int): Number of input channels.
+        num_heads (int): Number of attention heads.
+        mlp_ratio (float): Ratio of mlp hidden dim to embedding dim.
+        qkv_bias (bool, optional): If True, add a learnable bias to query, key, value. Default: True
+        drop_path (float, optional): Stochastic depth rate. Default: 0.0
+        act_layer (nn.Module, optional): Activation layer. Default: nn.GELU
+        norm_layer (nn.Module, optional): Normalization layer.  Default: nn.LayerNorm
+        ffn (bool): If False, pure attention network without FFNs
+    """
+    def __init__(self, dim, num_heads, mlp_ratio=4., qkv_bias=False,
+                 drop_path=0., act_layer=nn.GELU, norm_layer=nn.LayerNorm,
+                 ffn=True, type='channel'):
+        super().__init__()
+
+        self.cpe = nn.ModuleList([ConvPosEnc(dim=dim, k=3),
+                                  ConvPosEnc(dim=dim, k=3)])
+        self.ffn = ffn
+        self.norm1 = norm_layer(dim)
+        if type == 'channel':
+            self.attn = CrossChannelAttention(dim, num_heads=num_heads, qkv_bias=qkv_bias)
+        elif type == 'spatial':
+            self.attn = CrossSpatialAttention(dim, num_heads=num_heads, qkv_bias=qkv_bias)
+        else:
+            raise NotImplementedError
+        self.drop_path = DropPath(drop_path) if drop_path > 0. else nn.Identity()
+
+        if self.ffn:
+            self.norm2 = norm_layer(dim)
+            mlp_hidden_dim = int(dim * mlp_ratio)
+            self.mlp = Mlp(
+                in_features=dim,
+                hidden_features=mlp_hidden_dim,
+                act_layer=act_layer)
+
+    def forward(self, x, source, size):
+        x = self.cpe[0](x, size)
+        source = self.cpe[0](source, size)
+        # x = self.norm1(x)
+        # source = self.norm1(source)
+        cur = self.attn(self.norm1(x), self.norm1(source))
+        x = source + self.drop_path(cur)
+
+        x = self.cpe[1](x, size)
+        if self.ffn:
+            x = x + self.drop_path(self.mlp(self.norm2(x)))
+        return x, size
+    
+class AttentionBlock(nn.Module):
+    def __init__(self, dim, num_heads, windows_size):
+        super().__init__()
+
+        self.sb = SpatialBlock(dim=dim, num_heads=num_heads, window_size=windows_size)
+        self.cb = ChannelBlock(dim=dim, num_heads=num_heads)   
+        self.csb = CrossAttentionBlock(dim=dim, num_heads=num_heads, type="spatial")
+        self.ccb = CrossAttentionBlock(dim=dim, num_heads=num_heads, type="channel")
+
+    def forward(self, x, size): # b, c, h, w
+        is_list = isinstance(x, tuple) or isinstance(x, list)
+        if is_list:
+            batch_dim = x[0].shape[0]
+            x = torch.cat(x, dim=0)
+
+        x, size = self.sb(x, size)
+        x, size = self.cb(x, size)
+
+        if is_list:
+            x, source = torch.split(x, [batch_dim, batch_dim], dim=0)
+        
+        x_     , _ = self.csb(source, x, size)
+        source_, _ = self.ccb(x, source, size)
+
+        return x_, source_
+
+
+
+if __name__ == "__main__":
+    from einops import rearrange
+
+    h, w = 368//8, 496//8
+    fmap1 = torch.randn(1, 256, h, w)
+    fmap2 = torch.randn(1, 256, h, w)
+
+    # dpr = [x.item() for x in torch.linspace(0, 0.2, 6)]
+    # sb = SpatialBlock(dim=256, num_heads=8, window_size=7) # point-wise self-attention
+    # cb = ChannelBlock(dim=256, num_heads=8)                # channel-wise self-attention
+    ab = AttentionBlock(dim=256, num_heads=8, windows_size=7)
+
+    b, c, h, w = fmap1.shape
+    fmap1 = rearrange(fmap1, 'n c h w -> n (h w) c')
+    fmap2 = rearrange(fmap2, 'n c h w -> n (h w) c')
+
+    # fmap1_0, _ = cb(fmap2, fmap1, (h, w))
+    # fmap2_0, _ = cb(fmap1, fmap2, (h, w))
+    fmap1, fmap2 = ab([fmap1, fmap2], (h, w))
+
+    fmap1 = rearrange(fmap1, 'n (h w) c -> n c h w', h=h, w=w)
+    fmap2 = rearrange(fmap2, 'n (h w) c -> n c h w', h=h, w=w)
+
+    print(fmap1.shape)
